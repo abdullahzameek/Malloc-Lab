b43d644e6c117602d359da61e02e38391c76c059
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2018-12-02 16:55:10 +0400

    Moved to using word size

--- mm.c
+++ mm.c
@@ -18,21 +18,17 @@
 #include "mm.h"
 #include "memlib.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
-// team_t team = {
-//     /* Team name */
-//     "SegFault",
-//     /* First member's full name */
-//     "Lukas Zapolskas",
-//     /* First member's email address */
-//     "lz1477@nyu.edu",
-//     /* Second member's full name (leave blank if none) */
-//     "Abdullah Zameek",
-//     /* Second member's email address (leave blank if none) */
-//     "arz268@nyu.edu"};
+team_t team = {
+    /* Team name */
+    "SegFault",
+    /* First member's full name */
+    "Lukas Zapolskas",
+    /* First member's email address */
+    "lz1477@nyu.edu",
+    /* Second member's full name (leave blank if none) */
+    "Abdullah Zameek",
+    /* Second member's email address (leave blank if none) */
+    "arz268@nyu.edu"};
 
 /* Macros start here */
 
@@ -57,8 +53,8 @@
 #define HDRP(p) ((char *)(bp)-WSIZE)
 #define FTRP(p) ((char *)(bp) + GET_SIZE(HDRP(p)) - DSIZE)
 
-#define NEXT_BLKP(bp) ((char *) (bp) + GET_SIZE(((char *)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *) (bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
@@ -66,16 +62,14 @@
 
 /*Static global variable begin here */
 
-static char* heap_listp = NULL; //we use a char since a char is essentially a DWORD
+static char *heap_listp = NULL; //we use a char since a char is essentially a DWORD
 
 /* Static global variables end here */
 
 /* Support function def */
 
 static void *extend_heap(size_t words);
-static void *coalesce(void* bp);
-
-
+static void *coalesce(void *bp);
 
 /* End of Support Function Defs */
 
@@ -88,11 +82,16 @@ int mm_init(void)
     if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
         return -1;
 
-    PUT(heap_listp, 0);                            /* Alignment Padding*/
-    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); /* Prologue Heading */
-    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); /* Prologue Footer  */
-    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));     /* Epilogue Header  */
-    heap_listp += (2 * WSIZE);
+    // PUT(heap_listp, 0);                            /* Alignment Padding*/
+    // PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); /* Prologue Heading */
+    // PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); /* Prologue Footer  */
+    // PUT(heap_listp + (3 * WSIZE), PACK(0, 1));     /* Epilogue Header  */
+    // heap_listp += (2 * WSIZE);
+
+    PUT(heap_listp, PACK(0, 1));
+    PUT(heap_listp + (1 * WSIZE), PACK(0, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(0, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
 
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
@@ -106,15 +105,12 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-        return NULL;
-    else
+    if (size <= 0)
     {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+        return NULL;
     }
+
+    int newsize = ALIGN(size);
 }
 
 /*
@@ -122,7 +118,6 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
-
 }
 
 /*
@@ -164,35 +159,39 @@ static void *extend_heap(size_t words)
     return coalesce(bp);
 }
 
-static void *coalesce(void *bp) 
+static void *coalesce(void *bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
-    if (prev_alloc && next_alloc) {            /* Case 1 */
-    return bp;
+    if (prev_alloc && next_alloc)
+    { /* Case 1 */
+        return bp;
     }
 
-    else if (prev_alloc && !next_alloc) {      /* Case 2 */
-    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-    PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size,0));
+    else if (prev_alloc && !next_alloc)
+    { /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
     }
 
-    else if (!prev_alloc && next_alloc) {      /* Case 3 */
-    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-    PUT(FTRP(bp), PACK(size, 0));
-    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-    bp = PREV_BLKP(bp);
+    else if (!prev_alloc && next_alloc)
+    { /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
     }
 
-    else {                                     /* Case 4 */
-    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
-        GET_SIZE(FTRP(NEXT_BLKP(bp)));
-    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-    bp = PREV_BLKP(bp);
-    }   
+    else
+    { /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+                GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
     return bp;
 }
