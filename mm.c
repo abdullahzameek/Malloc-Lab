405c8b01cd983162a5b2ffc27da662b5a3f70432
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2018-12-05 09:56:57 +0400

    Converted some macros to static functions

--- mm.c
+++ mm.c
@@ -62,14 +62,44 @@ inline size_t get_class(size_t size) {
 }
 
 // TODO Figure out what the best way to convert these functions is
-#define GET(p) (*(unsigned int *)(p))
-#define PUT(p, val) (*(unsigned int *)(p) = (val))
+#define GET(p) (*(unsigned int *)(p)) // Get data from pointer location
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) // Asign data to pointer location
 
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
+#define PACK(size, alloc) (size | alloc)
+
+static inline size_t pack(size_t size, size_t alloc) {
+    return (size | alloc);  
+}
+
+// TODO Are we using void pointers everywhere, or is there something
+// I'm missing out on? 
+// TODO Check equivalence on working code
+static inline size_t get_size(void* bp) {
+    return (*(size_t*) bp) & ~0x7;
+}
+
+static inline size_t get_alloc(void *bp) {
+    return (*(size_t*) bp) & 0x1;
+}
+
+// Are these referencing a global variable bp?
+// If so, these
 #define HDRP(p) ((char *)(bp)-WSIZE)
 #define FTRP(p) ((char *)(bp) + GET_SIZE(HDRP(p)) - DSIZE)
+
+// TODO Casting
+static inline size_t header_pointer(void *bp) {
+    return ((char *) bp) - WSIZE;
+}
+
+// TODO Casting
+static inline footer_pointer(void *bp) {
+    return ((char *) bp);
+}
+
 
 #define NEXT_BLKP(bp) ((char *) (bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char *) (bp) - GET_SIZE(((char *)(bp) - DSIZE)))
@@ -78,11 +108,12 @@ inline size_t get_class(size_t size) {
 
 
 // Support function forward declaration
-static void *extend_heap(size_t words);
-static void *coalesce(void* bp);
+static void *extend_heap(size_t words); 
+static void *coalesce(void* bp); // TODO Do we coalesce on free, or do we coalesce on heap extension?
 static void mm_checker(void* bp);
 static void check_block(void* bp);
 static void best_fit(size_t size);      
+
 
 
 static char *heap_list = NULL;
@@ -107,8 +138,7 @@ int mm_init(void)
     PUT(heap_list + (3*WSIZE), PACK(0, 1));     // Epilogue footer - can be optimized out
     heap_list+= 2 * WSIZE;
 
-    // Extend heap
-
+    return NULL;
 }
 
 /* 
@@ -117,7 +147,7 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-
+    return NULL;
 }
 
 /*
@@ -125,7 +155,6 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
-
 }
 
 /*
@@ -133,16 +162,20 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-
+    return NULL;
 }
 
 static void *extend_heap(size_t words)
 {
+    // Extended words (even for boundary alignment)
+    size_t ewords = (words % 2 == 0) ? words : words + 1;
+
+    (size_t) mem_sbrk(ewords);
+
 
 }
 
 static void *coalesce(void *bp) 
 {
     
-
 }
